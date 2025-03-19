bc9eaa384a036ec73b9125158940363a288371c6
ericFatec <eric.paula@fatec.sp.gov.br>
2025-03-19 18:58:08 -0300

    fix: Movendo pastas

--- main/java/com/nectopoint/backend/exceptions/DuplicateException.java
+++
@@ -1,9 +0,0 @@
-package com.nectopoint.backend.exceptions;
-
-
-//Sendo utilizado para evitar CPF,EMAIL e Número de funcionário duplicados
-public class DuplicateException extends RuntimeException {
-    public DuplicateException(String message) {
-        super(message);
-    }
-}
